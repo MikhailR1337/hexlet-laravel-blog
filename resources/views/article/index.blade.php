0a413bde7ec7a9bcd9a3130ab7009aeaedd6f6e0
Mikhail Rogov <m.rogov@mel.fm>
2021-10-15 23:54:09 +0300

    bug fix

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <a href="{{ route('articles.show', ['article' => $article->id]) }}">
+        <a href="{{ route('articles.show', ['id' => $article->id]) }}">
             <h2>{{$article->name}}</h2>
         </a>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
-        <a href="{{ route('articles.edit', ['article' => $article->id]) }}">
+        <a href="{{ route('articles.edit', ['id' => $article->id]) }}">
             <h3>Изменить</h3>
         </a>
         {{ Form::model($article, ['url' => route('articles.destroy', $article), 'method' => 'DELETE']) }}
